d7842566a836a5dab96b0dd611df473a625369f9
Yiğit Küçük <yigitkucukcs@gmail.com>
2022-07-20 22:15:35 +0300

    Created interface
    Created interface

---
+++ app/src/main/java/com/example/penajamm/Navigation.java
@@ -0,0 +1,14 @@
+package com.example.penajamm;
+
+import android.content.Intent;
+
+public interface Navigation {
+
+    public void goSettings();
+
+    public void goMainScreen();
+
+    public void goProfile();
+
+    public void goPost();
+}
