376664eaf0a826e7e451f596c5cc95434b28fc00
Yiğit Küçük <yigitkucukcs@gmail.com>
2022-07-24 02:26:49 +0300

    Added UserSearchActivity
    Not yet functional

---
+++ app/src/main/java/com/example/penajamm/UserSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.penajamm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_search);
+    }
+}
