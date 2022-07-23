453d89fbcdd4ae1b6df4d779f8b628df25f6cc89
Yiğit Küçük <yigitkucukcs@gmail.com>
2022-07-23 18:34:40 +0300

    Verification fixed
    Verification Fixed
    
    Co-Authored-By: Alphan Tulukcu <91702196+alphantulukcu@users.noreply.github.com>
    Co-Authored-By: Defne Gürbüz <91427199+defnegrbz@users.noreply.github.com>
    Co-Authored-By: Barış Çelik <91495852+bariscelik123@users.noreply.github.com>
    Co-Authored-By: Özgür Ege Şire <95249765+ozguregesire@users.noreply.github.com>

---
+++ app/src/main/java/com/example/penajamm/VerificationActivity.java
@@ -0,0 +1,68 @@
+package com.example.penajamm;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class VerificationActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseUser firebaseUser;
+    Button verification_button;
+    TextView verification_text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        verification_button = findViewById(R.id.verification_button);
+        verification_text = findViewById(R.id.verification_text);
+
+        verification_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(VerificationActivity.this, "Verification E-mail Has Been Sent :) Check Your Spam Folder too!", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+            }
+        });
+
+        verification_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(VerificationActivity.this, LoginActivity.class));
+            }
+        });
+
+
+
+    }
+}
