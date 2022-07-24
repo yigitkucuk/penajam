aada8b03e51ac55c5356ccf1c0500185974ecba1
bariscelik123 <91495852+bariscelik123@users.noreply.github.com>
2022-07-24 03:20:16 +0300

    forgot password function added
    Co-Authored-By: Yiğit Küçük <91060732+YigitKucuk@users.noreply.github.com>
    Co-Authored-By: Defne Gürbüz <91427199+defnegrbz@users.noreply.github.com>
    Co-Authored-By: Alphan Tulukcu <91702196+alphantulukcu@users.noreply.github.com>
    Co-Authored-By: Özgür Ege Şire <95249765+ozguregesire@users.noreply.github.com>

--- app/src/main/java/com/example/penajamm/LoginActivity.java
+++ app/src/main/java/com/example/penajamm/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.example.penajamm;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,6 +22,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText email, password;
     private Button btnLogin;
     private TextView textRegister;
+
+    private TextView forgotTextLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +36,61 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.login);
         textRegister = findViewById(R.id.text_register);
 
+        forgotTextLink = findViewById(R.id.forgotPassword);
+
         btnLogin.setOnClickListener(view -> login());
 
         textRegister.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
+
+        //start of forgot password
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter your e-mail to receive reset link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                         String mail = resetMail.getText().toString();
+                         mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                             @Override
+                             public void onSuccess(Void unused) {
+
+                                 Toast.makeText(LoginActivity.this, "Reset link sent to your email.", Toast.LENGTH_SHORT).show();
+
+                             }
+                         }).addOnFailureListener(new OnFailureListener() {
+                             @Override
+                             public void onFailure(@NonNull Exception e) {
+
+                                 Toast.makeText(LoginActivity.this, "Error! Reset link is not sent." + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                              }
+                         });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                passwordResetDialog.create().show();
+            }
+        });
     }
+
+        //end of forgot password
+
 
     private void login() {
         String user = email.getText().toString().trim();
@@ -54,5 +114,9 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
         }
+
+
     }
+
+
 }
