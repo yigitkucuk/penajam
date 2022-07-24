35d3c4c0b4b23ae6ea0c5b7a565bb087e951d1bf
Yiğit Küçük <yigitkucukcs@gmail.com>
2022-07-24 22:59:37 +0300

    PrivateChatroom initial commit
    We will implement private chatroom.
    
    Co-Authored-By: Barış Çelik <91495852+bariscelik123@users.noreply.github.com>

---
+++ app/src/main/java/com/example/penajamm/PrivateChatroomActivity.java
@@ -0,0 +1,14 @@
+package com.example.penajamm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrivateChatroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_chatroom);
+    }
+}
